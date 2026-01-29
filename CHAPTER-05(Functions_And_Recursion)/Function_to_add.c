51bb380de6430c9f0c56754523f9df05476ced8b
Shubham yadav <shubhamyadavgpu@gmail.com>
2026-01-29 20:11:47 +0530

    Function and Recursion
    C program : Functions and Recursion

---
+++ CHAPTER-05(Functions_And_Recursion)/Function_to_add.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int sum(int , int);
+
+//Function definition
+int sum(int x, int y)
+{
+    printf("The sum is : %d\n", x+y);
+    return x+y;
+}
+
+int main ()
+{
+    int a,b;
+    a=5;
+    b=6;
+    sum(a,b);
+    return 0;
+}
