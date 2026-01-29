efdc6d6cc4a1a07bcb518484427dcdad858cfcaa
Shubham yadav <shubhamyadavgpu@gmail.com>
2026-01-29 10:32:30 +0530

    C programs: variables, constants & keywords.”
    “Basic C programs explaining variables, constants, and keywords for learners.”

---
+++ CHAPTER-01(Variables,constants_and_Keywords)/comment.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+    // this is a single line comment
+
+    /*
+      This is a multi-line comment
+      This comment spans multiple lines
+    */
+
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ CHAPTER-01(Variables,constants_and_Keywords)/variables.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+    int a;
+    a=6;
+    printf("the number is : %d",a);
+}
