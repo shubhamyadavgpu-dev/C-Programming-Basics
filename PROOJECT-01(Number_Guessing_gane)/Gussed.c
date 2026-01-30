00c09d1fc2f149ae7f37a1603e621adde15d6b65
Shubham yadav <shubhamyadavgpu@gmail.com>
2026-01-30 23:22:52 +0530

    Guess a number

---
+++ PROOJECT-01(Number_Guessing_gane)/Gussed.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    // Seed the random number generator with current time
+    srand(time(0));
+
+    // Generate random number between 1 and 100
+    int randomNumber = (rand() % 100) + 1;
+
+    // Print the random number
+    printf("Random number between 1 and 100: %d\n", randomNumber);
+
+    return 0;
+}
